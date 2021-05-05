a55cd86aef91eeea5a19f78eb8d2c5eec002584f
Deep Chatterjee <deep.chatterjee@ligo.org>
2021-05-05 16:49:00 -0500

    add custom mass prior objects

--- t_cosmo/custom_prior.py
+++ t_cosmo/custom_prior.py
@@ -1,5 +1,8 @@
-import h5py
+import io
+import os
+from urllib import parse, request
 
+import h5py
 import numpy as np
 import scipy as sp
 
@@ -68,3 +71,62 @@ class ZSquaredPrior(prior.Prior):
         norm = self.maximum**3 - self.minimum**3
         res = np.cbrt(p * norm + self.minimum**3)
         return res
+
+
+# create a custom mass prior for GW170817 masses
+_hdf5_url = 'https://dcc.ligo.org/public/0157/P1800370/005/GW170817_GWTC-1.hdf5'
+try:
+    fp = open(os.path.basename(parse.urlparse(_hdf5_url).path), "rb")
+except FileNotFoundError:
+    fp = io.BytesIO(request.urlopen(_hdf5_url).read())
+_posterior_sample_file = h5py.File(fp, 'r')
+
+_mass1_detector_frame = _posterior_sample_file[
+    'IMRPhenomPv2NRT_lowSpin_posterior']['m1_detector_frame_Msun']
+_mass2_detector_frame = _posterior_sample_file[
+    'IMRPhenomPv2NRT_lowSpin_posterior']['m2_detector_frame_Msun']
+
+# FIXME bins are hardcoded
+_mm, _bb = np.histogram(_mass1_detector_frame, bins=20)
+_freq_mass_1 = _mm/np.sum(_mm)
+_bin_centers_freq_mass_1 = 0.5*(_bb[1:] + _bb[:-1])
+
+_mm, _bb = np.histogram(_mass1_detector_frame, bins=100)
+_cum_freq_mass_1 = np.cumsum(_mm)/np.sum(_mm)
+_bin_centers_cum_freq_mass_1 = 0.5*(_bb[1:] + _bb[:-1])
+# detector frame mass_1 and mass_2 interpolants
+_m1_freq_interpolant = sp.interpolate.interp1d(
+    _bin_centers_freq_mass_1, _freq_mass_1,
+    fill_value=(0.,  0.), bounds_error=False
+)
+_m1_cum_freq_interpolant = sp.interpolate.interp1d(
+    _bin_centers_cum_freq_mass_1, _cum_freq_mass_1,
+    fill_value=(.0,  1.), bounds_error=False
+)
+_mm, _bb = np.histogram(_mass2_detector_frame, bins=20)
+_freq_mass_2 = _mm/np.sum(_mm)
+_bin_centers_freq_mass_2 = 0.5*(_bb[1:] + _bb[:-1])
+
+_mm, _bb = np.histogram(_mass2_detector_frame, bins=100)
+_cum_freq_mass_2 = np.cumsum(_mm)/np.sum(_mm)
+_bin_centers_cum_freq_mass_2 = 0.5*(_bb[1:] + _bb[:-1])
+
+_m2_freq_interpolant = sp.interpolate.interp1d(
+    _bin_centers_freq_mass_2, _freq_mass_2,
+    fill_value=(0.,  0.), bounds_error=False
+)
+_m2_cum_freq_interpolant = sp.interpolate.interp1d(
+    _bin_centers_cum_freq_mass_2, _cum_freq_mass_2,
+    fill_value=(0.,  1.), bounds_error=False
+)
+
+mass_1_prior_gw170817 = CustomMassPrior(
+    _m1_freq_interpolant,
+    _m1_cum_freq_interpolant
+)
+"""Primary mass prior using GW170817 posterior samples."""
+mass_2_prior_gw170817 = CustomMassPrior(
+    _m2_freq_interpolant,
+    _m2_cum_freq_interpolant
+)
+"""Secondary mass prior using GW170817 posterior samples."""
